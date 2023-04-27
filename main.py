be99f9b636a8defae4058180355466d0d41772a2
Vasil Ivanov <ivanovvasil99@gmail.com>
2023-04-27 22:58:38 +0300

    Add center window cropping

--- main.py
+++ main.py
@@ -6,16 +6,25 @@ from pathvalidate.argparse import validate_filepath_arg
 from skimage.metrics import structural_similarity as ssim
 
 
-def calculate_frame_similarity(frame_a, frame_b):
+def calculate_frame_similarity(frame_a, frame_b, crop_side = 0):
     a = frame_a.to_ndarray(format="rgb24")
     b = frame_b.to_ndarray(format="rgb24")
+
+    if crop_side != 0:
+        a = crop_center(a, crop_side)
+        b = crop_center(b, crop_side)
     return ssim(a, b, channel_axis=2)
 
+def crop_center(img, crop_side):
+    y,x, _ = img.shape
+    startx = x//2-(crop_side//2)
+    starty = y//2-(crop_side//2)    
+    return img[starty:starty+crop_side,startx:startx+crop_side, :]
 
 def calculate_ssim_score(video_a_path,
                          video_b_path,
                          n_skip_frames=30,
-                         max_frames=None):
+                         max_frames=None, crop_side = 0):
     vid_a_ctr = av.open(video_a_path)
     vid_b_ctr = av.open(video_b_path)
 
@@ -33,7 +42,7 @@ def calculate_ssim_score(video_a_path,
             break
         if idx % n_skip_frames != 0:
             continue
-        ssim_results.append(calculate_frame_similarity(frame_a, frame_b))
+        ssim_results.append(calculate_frame_similarity(frame_a, frame_b, crop_side))
         frame_numbers.append(idx)
 
     vid_a_frame_iter = np.array(frame_numbers)
@@ -67,7 +76,13 @@ def parse_cli():
         type=int,
         default=200_000,
         help="Maximum number of frames to be processed. [Default: 200000]")
-
+    
+    parser.add_argument(
+        '-c',
+        '--crop_side',
+        type=int,
+        default=0,
+        help="Crop a center square from the video with the specified side. 0 means don't crop. [Default: 0]")
     options = parser.parse_args()
 
     return options
@@ -76,7 +91,8 @@ def parse_cli():
 @Gooey(
     program_name="Source-encode SSIM calculator",
     program_description=
-    "Calculates the SSIM score frame by frame between a source and encode.\nRequires both to have the same resolution and number of frames"
+    "Calculates the SSIM score frame by frame between a source and encode.\nRequires both to have the same resolution and number of frames",
+    default_size=(610, 610)
 )
 def main():
     cli = parse_cli()
@@ -86,9 +102,12 @@ def main():
     frames, scores = calculate_ssim_score(cli.source_path,
                                    cli.encode_path,
                                    n_skip_frames=cli.num_skip_frames,
-                                   max_frames=cli.max_num_frames + 1)
+                                   max_frames=cli.max_num_frames + 1,
+                                   crop_side=cli.crop_side)
+    avg_score = np.average(scores)
     plt.plot(frames, scores, label = "Current SSIM score")
-    plt.hlines([np.average(scores)], xmin=frames[0], xmax=frames[-1], colors = ["red"], label="Average SSIM score over range")
+    plt.hlines([avg_score], xmin=frames[0], xmax=frames[-1], colors = ["red"], label=f"Average SSIM score: {avg_score:.3f}")
+    print(f"Average SSIM score for whole video: {avg_score}")
     plt.legend()
     plt.show()
 
